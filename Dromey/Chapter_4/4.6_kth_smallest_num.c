91a13aa957e834e5fba02e93e1a006488b3b3d52
Dinesh Thirumurthy <dinesh@hakr.co.in>
2017-10-05 15:58:33 +0530

    indentation of indexes / loop conditions

--- Dromey/Chapter_4/4.6_kth_smallest_num.c
+++ Dromey/Chapter_4/4.6_kth_smallest_num.c
@@ -26,11 +26,11 @@ int main()
 	scanf("%d", &x);
 	
 	for(i = 0; i < n; ++i){
-		for(j = 0; j < n - i - 1; ++j){
-			if(a[j] > a[j + 1]){
+		for(j = 0; j < n-i-1; ++j){
+			if(a[j] > a[j+1]){
 				t = a[j];
-				a[j] = a[j + 1];
-				a[j + 1] = t;
+				a[j] = a[j+1];
+				a[j+1] = t;
 			}
 		}
 	}
